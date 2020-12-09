018fe68edff2f4ee916a33dcdf136a600373528c
rabbykhairul <rabbykhairul@gmail.com>
2020-12-09 16:57:53 +0600

    successfully store the uploaded image sent in '/api/resize-image' endpoint

--- src/api/resize-image.js
+++ src/api/resize-image.js
@@ -1,8 +1,26 @@
 const express = require("express");
 const router = express.Router();
+const multer = require("multer");
+const path = require("path");
 
-router.post("/", (req, res) => {
-  res.send({ message: "Response received on `resizeImage` api endpoint." });
+const IMAGE_UPLOAD_PATH = "../../files/resize-image/uploaded-images/";
+
+const uploadImageStorage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, path.join(__dirname, IMAGE_UPLOAD_PATH));
+  },
+
+  filename: (req, file, cb) => {
+    const newFileName = `${Date.now()}-${Math.round(Math.random() * 1e9)}`;
+    const fileExtension = path.extname(file.originalname);
+    cb(null, `${newFileName}${fileExtension}`);
+  },
+});
+
+const upload = multer({ storage: uploadImageStorage });
+
+router.post("/", upload.single("image"), (req, res) => {
+  res.send({ uploadedImage: req.file.originalname });
 });
 
 module.exports = router;
